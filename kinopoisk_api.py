fde9dce659d6eb45715467eb78737a691d2452a5
SoloMen <solomen88@ya.ru>
2024-08-24 20:51:34 +0300

    Оптимизировал модуль kinopoisk_api, убрал лишние методы и классы.

--- kinopoisk_api.py
+++ kinopoisk_api.py
@@ -34,22 +34,6 @@ class FILM:
                 self.actors.append(person['nameRu'])
 
 
-class SEARCH:
-    def __init__(self, data: dict):
-        self.kp_id = data['filmId']
-        self.name = data['nameRu'] if data['nameEn'] == '' else data['nameEn']
-        self.ru_name = data['nameRu']
-        self.year = data['year'].split('-')[0]
-        self.duration = data['filmLength']
-        self.genres = [genre['genre'] for genre in data['genres']]
-        self.countries = [country['country'] for country in data['countries']]
-        self.kp_rate = data['rating']
-        self.kp_url = f'https://www.kinopoisk.ru/film/{data["filmId"]}/'
-        self.poster = data['posterUrl']
-        self.poster_preview = data['posterUrlPreview']
-        self.description = data['description']
-
-
 class KP:
     def __init__(self, token, secret=None):
         self.token = token
@@ -66,22 +50,10 @@ class KP:
     def get_film(self, film_id):
         cache = CACHE().load()
 
-        # rate_request = requests.get(f'https://rating.kinopoisk.ru/{film_id}.xml').text
-        # try:
-        #     kp_rate = xml.fromstring(rate_request)[0].text
-        # except IndexError:
-        #     kp_rate = 0
-        # try:
-        #     imdb_rate = xml.fromstring(rate_request)[1].text
-        # except IndexError:
-        #     imdb_rate = 0
-
         if str(film_id) in cache:
             data = {}
             for a in cache[str(film_id)]:
                 data[a] = cache[str(film_id)][a]
-            # data['kp_rate'] = kp_rate
-            # data['imdb_rate'] = imdb_rate
             return FILM(data)
 
         for _ in range(10):
@@ -89,8 +61,6 @@ class KP:
                 request = requests.get(
                     self.API + 'films/' + str(film_id), headers=self.headers)
                 request_json = json.loads(request.text)
-                # request_json['data']['kp_rate'] = kp_rate
-                # request_json['data']['imdb_rate'] = imdb_rate
                 cache[str(film_id)] = request_json
                 request_staff = requests.get(
                     self.API_staff + 'staff?filmId=' + str(film_id), headers=self.headers)
@@ -98,38 +68,6 @@ class KP:
                 cache[str(film_id)].update({'staff': request_staff_json})
                 CACHE().write(cache)
                 return FILM(cache[str(film_id)])
-            except json.decoder.JSONDecodeError:
-                time.sleep(0.5)
-                continue
-
-    def search(self, query):
-        for _ in range(10):
-            try:
-                request = requests.get(self.API + 'films/search-by-keyword', headers=self.headers,
-                                       params={"keyword": query, "page": 1})
-                request_json = json.loads(request.text)
-                output = []
-                for film in request_json['films']:
-                    try:
-                        output.append(SEARCH(film))
-                    except (Exception, BaseException):
-                        continue
-                return output
-            except json.decoder.JSONDecodeError:
-                time.sleep(0.5)
-                continue
-
-    def top500(self):
-        for _ in range(10):
-            try:
-                request = requests.get(self.API + 'films/top?type=BEST_FILMS_LIST&page=1&listId=1',
-                                       headers=self.headers
-                                       )
-                request_json = json.loads(request.text)
-                output = []
-                for film in request_json['films']:
-                    output.append(SEARCH(film))
-                return output
             except json.decoder.JSONDecodeError:
                 time.sleep(0.5)
                 continue
